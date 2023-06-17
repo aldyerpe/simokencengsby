17efcd051a8f853f3251752aa2c8655b9079194e
AAKEN Programmer <aakenprogrammer@gmail.com>
2023-06-17 23:16:47 +0700

    Adding Toast for Success and Error

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,26 @@ export default function App() {
     createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
+      setUserRegister(user.email);
+      setTimeout(() => {
+        setUserRegister('');
+      }, 15000)
     })
     .catch((error) => {
     const errorCode = error.code;
-    const errorMessage = error.message;
+    console.log(error.code);
+    switch (error.code){
+      case 'auth/email-already-in-use':
+      setUserError('Email Sudah Digunakan, Silahkan Masuk');
+      setTimeout(() => {
+        setUserError('');
+      }, 15000)
+      break;
+    }
     })
   }
+  const [userRegister, setUserRegister] = useState('');
+  const [userError, setUserError] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   return (
@@ -50,6 +64,22 @@ export default function App() {
               <Button tema='bg-[#3377ff] text-white' label='Daftar' tipe='button' onClick={daftarUser} />
             </div>
           </form>
+          {userRegister !== null && userRegister !== "" ? (
+          <div className='absolute top-[-300px] left-0 right-0 bg-white w-[90%] mx-auto rounded-md flex flex-col items-center text-center gap-y-2 p-4 animasi_slideDown'>
+            <p><span className='bg-[#3377ff] px-1 py-.5 text-white font-bold'>{userRegister}</span><br />Berhasil didaftarkan di sistem kami. Silahkan masuk menggunakan email yang sudah terdaftar.</p>
+          </div>
+          ) : (
+          <></>
+          )
+          }
+          {userError !== null && userError !== "" ? (
+          <div className='absolute top-[-300px] left-0 right-0 bg-white w-[90%] mx-auto rounded-md flex flex-col items-center text-center gap-y-2 p-4 animasi_slideDown'>
+            <p><span className='bg-[#ff3377] px-1 py-.5 text-white font-bold'>{userError}</span></p>
+          </div>
+          ) : (
+          <></>
+          )
+          }
           <div className='bg-white w-full mt-auto flex items-center justify-center'>
             <p className='font-semibold text-center'>&copy;2023 by<br /><a className='font-bold text-lg' href='https://github.com/aldyerpe' target='_blank'>Simo Kenceng Surabaya IT Team</a></p>
           </div>
